d33c916af28d44597a688cb37e10ca6ac6f8ad4e
mufdvr <belora2010@yandex.ru>
2018-11-27 12:24:39 +0300

    add html code editor

--- src/components/PublicEditor/PublicEditor.js
+++ src/components/PublicEditor/PublicEditor.js
@@ -19,13 +19,30 @@ import styles from './PublicEditorStyles'
 import { Snackbars, FileFields } from 'components'
 import { base64Loader } from 'utils'
 
+const htmlToEditorState = html => {
+  const contentBlock = htmlToDraft(html)
+  if (contentBlock) {
+    const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks)
+    return EditorState.createWithContent(contentState)
+  }
+}
+
+const colors = ['rgba(0,0,0,0)', '#ffc0d2', '#f6dfc4', '#f9b89c', '#45312b', '#bba080', '#d4b58e',
+    'rgba(69, 49, 43, 0.7)', 'rgb(26,188,156)', 'rgb(84,172,210)', 'rgb(44,130,201)', 'rgb(97,189,109)',
+    'rgb(147,101,184)', 'rgb(71,85,119)', 'rgb(204,204,204)', 'rgb(65,168,95)', 'rgb(0,168,133)',
+    'rgb(61,142,185)', 'rgb(41,105,176)', 'rgb(85,57,130)', 'rgb(40,50,78)', 'rgb(0,0,0)',
+    'rgb(247,218,100)', 'rgb(251,160,38)', 'rgb(235,107,86)', 'rgb(226,80,65)', 'rgb(163,143,132)',
+    'rgb(239,239,239)', 'rgb(255,255,255)', 'rgb(250,197,28)', 'rgb(243,121,52)', 'rgb(209,72,65)',
+    'rgb(184,49,47)', 'rgb(124,112,107)', 'rgb(209,213,216)']
+
 class PublicEditor extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
       editorState: EditorState.createEmpty(),
-      item: props.handleInit()
+      item: props.handleInit(),
+      codeView: false
     }
   }
 
@@ -43,10 +60,10 @@ class PublicEditor extends React.Component {
   }
 
   handleSubmit = () => {
-    const { editorState } = this.state
+    const { editorState, codeView } = this.state
     let item = {
       ...this.state.item,
-      body: draftToHtml(convertToRaw(editorState.getCurrentContent()))
+      body: codeView ? this.state.item.body : draftToHtml(convertToRaw(editorState.getCurrentContent()))
     } //так нужно для загрузки файлов
     const { handleCreate } = this.props
     base64Loader(item.blobImageAttributes)
@@ -70,24 +87,38 @@ class PublicEditor extends React.Component {
       return
     }
     if (!fetching && !errors.msg) {
-      const contentBlock = htmlToDraft(item.body)
-      if (contentBlock) {
-        const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks)
-        const editorState = EditorState.createWithContent(contentState)
-        this.setState(prev => ({
-          ...prev,
-          item: handleInit(item),
-          editorState
-        }))
-      }
+      this.setState(prev => ({
+        ...prev,
+        item: handleInit(item),
+        editorState: htmlToEditorState(item.body)
+      }))
     } else {
       errors.msg && Snackbars.error(errors.msg)
     }
   }
 
+  handleChangeView = () => {
+    const { codeView, editorState, item: { body } } = this.state
+    if (codeView) {
+      this.setState({
+        codeView: !codeView,
+        editorState: htmlToEditorState(body)
+      })
+    } else {
+      this.setState(prev => ({
+        ...prev,
+        codeView: !codeView,
+        item: {
+          ...prev.item,
+          body: draftToHtml(convertToRaw(editorState.getCurrentContent()))
+        }
+      }))
+    }
+  }
+
   render = () => {
     const { classes, fetching } = this.props
-    const { editorState, item: { title, description, blobImageAttributes, hidden, id } } = this.state
+    const { editorState, codeView, item: { title, description, blobImageAttributes, hidden, body, id } } = this.state
     return (
       <Paper className={classes.root} elevation={1}>
         <Typography variant="headline" component="h3">
@@ -119,16 +150,29 @@ class PublicEditor extends React.Component {
           />
         </div>
         <div className="content-wrapper">
-          <Editor
-            editorState={editorState}
-            wrapperClassName="editor-wrapper"
-            editorClassName="editor"
-            toolbarClassName="toolbar-class"
-            onEditorStateChange={this.onEditorStateChange}
-            localization={{
-              locale: 'ru',
-            }}
-          />
+          {
+            codeView ?
+              <textarea
+                style={{ margin: "0 8px" }}
+                onChange={this.handleChange}
+                rows="20"
+                name="body"
+                value={body}
+              /> :
+              <Editor
+                editorState={editorState}
+                wrapperClassName="editor-wrapper"
+                editorClassName="editor"
+                toolbarClassName="toolbar-class"
+                onEditorStateChange={this.onEditorStateChange}
+                toolbar={{
+                  colorPicker: { colors } 
+                }}
+                localization={{
+                  locale: 'ru',
+                }}
+              />
+          }
           <div className="news-buttons">
             <input
               onChange={event => this.handleChange({ blobImageAttributes: [event.target.files[0]] })}
@@ -145,13 +189,24 @@ class PublicEditor extends React.Component {
                 component="span"
                 className={classes.button}
               >
-                { blobImageAttributes.length ? blobImageAttributes[0].name : "Загрузить фото" }
+                {blobImageAttributes.length ? blobImageAttributes[0].name : "Загрузить фото"}
               </Button>
             </label>
+            <FormControlLabel
+              control={
+                <Switch
+                  onChange={this.handleChangeView}
+                  checked={codeView}
+                  name="html"
+                  color="primary"
+                />
+              }
+              label="HTML"
+            />
             <h4 className="file-fields-label">Фотогаллерея</h4>
             <FileFields name="blobGalleriesAttributes" onChange={this.handleChange} />
-            <div style={{display: 'flex'}}>
-              <div style={{position: 'relative'}}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ position: 'relative' }}>
                 <Button
                   onClick={this.handleSubmit}
                   variant="outlined"
