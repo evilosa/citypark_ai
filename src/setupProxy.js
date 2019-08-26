c69d7a8cb7b314e806ac6ebf5d380edbf4406433
evilosa <abo@lix.com>
2019-08-26 12:11:30 +0300

    Update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,5 +4,5 @@ module.exports = (app) => {
   app.use(proxy('/api/v1', { target: 'http://localhost:4000' }));
   app.use(proxy('/uploads', { target: 'http://localhost:4000' }));
   app.use(proxy('/onec', { target: 'http://85.172.104.69:8080' }));
-  app.use(proxy('/onec_restaurant', { target: 'http://85.172.104.69:8080' }));
+  app.use(proxy('/onec_hotel', { target: 'http://85.172.104.69:8080' }));
 };
