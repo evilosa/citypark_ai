826d5352160e65b5b167df9bc78c8560b2527f59
evilosa <abo@lix.com>
2019-08-26 12:50:19 +0300

    Fix missed envs

--- nginx/start.sh
+++ nginx/start.sh
@@ -8,7 +8,7 @@ echo "ONEC_GATEWAY": $ONEC_GATEWAY
 echo "API_ONEC_PLACEHOLDER": $API_ONEC_PLACEHOLDER
 
 # replace env for nginx conf
-envsubst '$API_PLACEHOLDER $UPLOADS_PLACEHOLDER $BACKEND_GATEWAY' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$API_PLACEHOLDER $UPLOADS_PLACEHOLDER $BACKEND_GATEWAY $ONEC_GATEWAY $API_ONEC_PLACEHOLDER' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # start nginx
 nginx -g 'daemon off;'
